bf0f19eee820d8a5f33b98db0fe8f8acebeac482
Kieu Nguyen <96586932+ktmindi@users.noreply.github.com>
2022-12-20 05:21:17 -0800

    up to leap year challenge

--- fundamentals/fundtodos.js
+++ fundamentals/fundtodos.js
@@ -32,27 +32,59 @@ for (var i = 1; i<99; i++){
 
 //Multiples of Three – but Not All
 //Using FOR, print multiples of 3 from -300 to 0. Skip -3 and -6.
-for (var i=-300; i<=0; i++){
-    if(i%-3==0 && i!=-3 && i!=-6){
-        console.log(i);
+for (var ii=-300; ii<=0; ii++){
+    if(ii%-3==0 && ii!=-3 && ii!=-6){
+        console.log(ii);
     }
 }
  
 
 //Printing Integers with While
 //Print integers from 2000 to 5280, using a WHILE.
-
-
+var num1 = 2000;
+while (num1<=5280){
+    console.log(num1);
+    num1 = num1+1;
+}
 
 //You Say It’s Your Birthday
 //If 2 given numbers represent your birth month and day in either order, log "How did you know?", else log "Just another day...." 
 
- 
+
+function birthday(num3, num2){
+    let month = 7;
+    let day = 9;
+    if(num3===month && num2===day){
+        console.log("How did you know?");
+    }else if(num3===day && num2===month){
+        console.log("How did you know?");
+    }else{
+        console.log("Just another day");
+    }
+}
+console.log(birthday(10,1));
+console.log(birthday(7,9));
+console.log(birthday(9,7));
 
 //Leap Year
 //Write a function that determines whether a given year is a leap year. If a year is divisible by four, it is a leap year, unless it is divisible by 100. However, if it is divisible by 400, then it is.
+function leapYear(year){
+    if(year%400==0){
+        return console.log(year, "is a leap year");
+    } else if(year %100 === 0){
+        return console.log(year, "is not a leap year");
+    } else if(year %4 ===0) {
+        return console.log(year, "is a leap year");
+    } else {
+        return console.log(year, "is not a leap year");
+    }
+}
 
-
+leapYear(2020);
+leapYear(2021);
+leapYear(2000);
+leapYear(1900);
+//By adding in return to the function.. we dont need to console log when we call the function. by leaving it as is for the birthday function, you will notice that the terminal returns "Undefined" after each test
 
 //Print and Count
 //Print all integer multiples of 5, from 512 to 4096. Afterward, also log how many there were.
