d5e8b3cf3438524a154c20ffd642403bf813d96e
Kieu Nguyen <96586932+ktmindi@users.noreply.github.com>
2023-01-06 02:35:04 -0800

    counting ways to reach top of stairs

--- practice/recursion.js
+++ practice/recursion.js
@@ -101,3 +101,19 @@ function findInOrderedSet(arr, target, left=0, right=arr.length-1) {
 const nums = [1, 4, 6, 7, 9, 17, 45];
 console.log(findInOrderedSet(nums, 4));  //-> true
 console.log(findInOrderedSet(nums, 2));  //-> false
+
+
+//Challenge 8 
+function countWaysToReachNthStair(n) {
+	if(n===1){
+  	return 1
+	}
+  if(n===2){
+    return 2
+  }
+  return countWaysToReachNthStair(n-1) + countWaysToReachNthStair(n-2)
+}
+
+console.log(countWaysToReachNthStair(1)) //-> 1 (only one way to climb 1 stair)
+console.log(countWaysToReachNthStair(2)) //-> 2 ((1, 1), (2))
+console.log(countWaysToReachNthStair(4)) //-> 5 ((1, 1, 1, 1), (1, 1, 2), (2, 1, 1), (2, 2))
