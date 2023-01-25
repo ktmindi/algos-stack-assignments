c901708c660e94d81337e218cf31bb05d9b8773f
Kieu Nguyen <96586932+ktmindi@users.noreply.github.com>
2023-01-25 03:14:19 -0800

    file creation

---
+++ notes/callbacksHigherOrderFunctions.md
@@ -0,0 +1,22 @@
+# Callbacks & Higher-order Functions
+### Functions like map, reduce and filter are powerful tools and keep our code DRY but can be complex to navigate. We will learn how to build these functions from scratch so you can easily implement and debug them in your code.
+---
+
+
+
+
+
+### H3
+## H2
+# H1
+~~STRIKE THROUGH~~
+**Bolded**
+
+*Italicized*
+`highlighted words`
+ - D
+ >Quoted Block
+
+ ```JAVASCRIPT
+var myName JAVASCRIPT CODE
+ ```
--- practice/practice.js
+++ practice/practice.js
@@ -21,11 +21,66 @@
 // }
 // console.log(txt);
 
-function makeItBig(array){
-    for(let i=0; i<array.length; i++){
-        if(array[i]>0){
-            array[i] = "big";
-        }
-    } return array;
-}
-console.log(makeItBig([-1,3,5,-5]));
+// function makeItBig(array){
+//     for(let i=0; i<array.length; i++){
+//         if(array[i]>0){
+//             array[i] = "big";
+//         }
+//     } return array;
+// }
+// console.log(makeItBig([-1,3,5,-5]));
+
+// function printOneReturnAnother(array){
+//     console.log(array[array.length-2]);
+//     //iterate through the array 
+//     for (let i =0; i<array.length;i++){
+//         if (array[i]%2 != 0){
+//             return array[i];
+//         }else{
+//             i++;
+//         }
+//     }
+//     //print second to last value in array
+//     //return the first odd number in the array
+//   }
+  //test case
+//   printOneReturnAnother([1,6,3,2,5,0,2,19]) //print 2 - return 1
+  
+
+
+//   function isOdd(num){
+//     if (num % 2 != 0){
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+//   console.log(isOdd(5)); // should log: true
+//   console.log(isOdd(2008)); // should log: false
+
+
+// function doubleVision(array){
+//     let newArray = [];
+//     for (let i=0; i<array.length; i++){
+//       newArray.push(array[i]*2);
+//     }
+//     console.log(array);
+//     return newArray;
+//   }
+  
+//   //test case
+// console.log(doubleVision([1,2,3])) //should return [2.4.6]
+
+function countPositives(arr){
+    let count = 0;
+    for (let i = 0; i<arr.length; i++){
+      if (arr[i]>0){
+        count +=1;
+      } else {
+        count = count;
+      }
+    arr[arr.length] = count;
+    }
+    return arr;
+  }
+countPositives([-1,1,1,1]) 
