70460d05ee707e6e9b39d410ca2d09f638e91ced
Kieu Nguyen <96586932+ktmindi@users.noreply.github.com>
2023-03-13 23:07:38 -0700

    set up array to do 4

---
+++ arrays/creditCardVal.js
@@ -0,0 +1,10 @@
+//Credit Card Validation
+
+// The Luhn formula is sometimes used to validate credit card numbers. Create the function isCreditCardValid(digitArr) that accepts an array of digits on the card (13-19 depending on the card), and returns a boolean whether the card digits satisfy the Luhn formula, as follows:
+
+// Set aside the last digit; do not include it in these calculations (until step 5);
+// Starting from the back, multiply the digits in odd positions (last, third-to-last, etc.) by 2;
+// If any results are larger than 9, subtract 9 from them;
+// Add all numbers (not just our odds) together;
+// Now add the last digit back in – the sum should be a multiple of 10.
+// For example, when given digit array [5,2,2,8,2], after step 1) it becomes [5,2,2,8], then after step 2) it is [5,4,2,16]. Post-3) we have [5,4,2,7], then following 4) it becomes 18. After step 5) our value is 20, so ultimately we return true. If the final digit were any non-multiple-of-10, we would instead return false.
